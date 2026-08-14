a1046a2a440c5717da333d3415377765f333396d
Oleksii Kurganovycgh <alexey.kurganovich@gmail.com>
2026-08-14 16:44:40 +0200

    Fix typo 3

--- kms.tf
+++ kms.tf
@@ -27,7 +27,7 @@ resource "aws_kms_key_policy" "ebs" {
         Effect = "Allow"
 
         Principal = {
-          AWS = "arn:aws:iam:${data.aws_caller_identity.current.account_id}:root"
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
         }
 
         Action   = "kms:*"
