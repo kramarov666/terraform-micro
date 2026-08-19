b0bff7a2587dde41ae6cb85ffd797b60b9062a0a
Oleksii Kurganovycgh <alexey.kurganovich@gmail.com>
2026-08-19 12:03:27 +0200

    Use cache for terraform

--- providers.tf
+++ providers.tf
@@ -13,12 +13,4 @@ terraform {
     region = "us-east-1"
   }
 
-  required_version = ">= 1.15.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 6.57"
-    }
-  }
 }
